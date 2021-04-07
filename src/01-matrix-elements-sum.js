fda5b6370d0495a8932835dc935c74cca23b15b2
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-04-07 20:29:15 +0300

    feat: add logic in 01-matrix-elements-sum.js

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,19 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+
+  function func (e, i, arr) {
+    e.forEach((elem, index) => {
+      if ((i === 0) || (arr[i - 1][index] !== 0)) {
+        sum += elem;
+      }
+    });
+  }
+
+  matrix.forEach(func);
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
