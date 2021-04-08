bf1e8ccebdec6031b289b4e2123e2b789b327191
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-04-08 09:52:44 +0300

    feat: add logic in src/03-sort-by-height.js

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,34 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const arr2 = [];
+  let index = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    arr2.push(arr[i]);
+  }
+
+  function sortSort (a, b) {
+    return a - b;
+  }
+
+  arr.sort(sortSort);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      arr.shift();
+      i -= 1;
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] !== -1) {
+      arr2[i] = arr[index];
+      index += 1;
+    }
+  }
+  return arr2;
 }
 
 module.exports = sortByHeight;
