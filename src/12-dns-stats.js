53e130a955e8200a59de4a847ac29de11b3ac1d8
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-04-12 09:48:04 +0300

    feat: add logic in src/12-dns-stats.js

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,45 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const arr = [];
+  const arrBuffer = [];
+  const obj = {};
+
+  let indexLongElem = 0;
+
+  if (domains.length === 0 || domains === undefined || !Array.isArray(domains)) { return obj; }
+
+  for (let i = 0; i < domains.length; i++) {
+    arr.push(domains[i].split('.'));
+  }
+
+  if (arr.length === 1) {
+    arrBuffer.push(0);
+  } else {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i].length >= arr[i + 1].length) {
+        arrBuffer.push(i);
+      } else {
+        arrBuffer.push(i + 1);
+      }
+    }
+  }
+
+  for (let x = 0; x < arrBuffer.length; x++) {
+    let str = '';
+    indexLongElem = arrBuffer[x];
+    for (let j = arr[indexLongElem].length - 1; j >= 0; j--) {
+      str += `.${arr[indexLongElem][j]}`;
+      obj[str] = 0;
+      for (let i = 0; i < domains.length; i++) {
+        if (domains[i].indexOf(arr[indexLongElem][j]) !== -1) {
+          obj[str] += 1;
+        }
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
